2997c44cf0f0656f81d18bfdfee7b151f7962699
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-01-17 11:20:45 -0500

    update indentation

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,9 @@
 ## Use this helper to format all error / warning / message text
 .msg <-
     function(fmt, ..., width=getOption("width"))
-    {
-        strwrap(sprintf(fmt, ...), width=width, exdent=4)
-    }
+{
+    strwrap(sprintf(fmt, ...), width=width, exdent=4)
+}
 
 .check_remote_exists <- function(remotes, remote) {
     remote %in% remotes[["name"]]
