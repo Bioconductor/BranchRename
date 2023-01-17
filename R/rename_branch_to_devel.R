3a344759aaea0fa4b1d2c7f7f57fc2a904ed8672
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-01-17 14:53:43 -0500

    add update_local_repo (singular) fx

--- R/rename_branch_to_devel.R
+++ R/rename_branch_to_devel.R
@@ -311,19 +311,26 @@ rename_branch_repos <- function(
 
 #' Update local repositories after GitHub branch rename
 #'
-#' This function works _only_ after renaming branches on GitHub from `master` to
-#' `devel`. It updates local folders so that branches are renamed to `devel`
+#' These functions works _only_ after renaming branches on GitHub from `master`
+#' to `devel`. It updates local folders so that branches are renamed to `devel`
 #' locally. It also ensures that the `HEAD` is pointing to the new remote
-#' location, i.e., `origin/devel` (see `gert::git_remote_info()$head`).
+#' location, i.e., `origin/devel` (see `gert::git_remote_info()$head`). For
+#' convenience, the singular `update_local_repo` function will update a
+#' particular local repository on the user's system.
 #'
-#' @details Note that the function queries the GitHub API to discover
-#'   repositories whose default branches are set to `devel`. The function then
-#'   matches those repository names with local folder names under `basedir`
-#'   and performs the necessary steps to rename the branch and set the
-#'   `HEAD` to `origin/devel`.
+#' @details Note that `update_local_repos` (plural) queries the GitHub API to
+#'   discover repositories whose default branches are set to `devel`. The
+#'   function then matches those repository names with local folder names under
+#'   `basedir` and performs the necessary steps to rename the branch and set the
+#'   `HEAD` to `origin/devel`. The singular function `update_local_repo` assumes
+#'   that the remote change has been made and will rename the local branch and
+#'   update the local repository to match the remote 'HEAD'.
 #'
 #' @param basedir `character(1)` The base directory where all packages /
 #'   repositories exist for the user
+#' 
+#' @param repo_dir `character(1)` The full path to the directory of the local
+#'   repository whose default branch should be updated
 #'
 #' @param username `character(1)` (optional) The GitHub username used in the query
 #'   to check default packages
@@ -344,15 +351,16 @@ update_local_repos <- function(
     repos <- repos[repos == new_branch]
     folders <- list.dirs(basedir, recursive = FALSE)
     fnames <- basename(folders)
-    matching <- intersect(folders, names(repos))
+    matching <- intersect(fnames, names(repos))
+    pkg_dirs <- file.path(basedir, matching)
+    
     if (!length(matching))
         stop("No local folders in 'basedir' to update")
 
     mapply(
-        FUN = .update_local_repo,
-        folder = matching,
+        FUN = update_local_repo,
+        package_dir = pkg_dirs,
         MoreArgs = list(
-            basedir = basedir,
             new_branch = new_branch,
             set_upstream = set_upstream
         ),
@@ -360,10 +368,13 @@ update_local_repos <- function(
     )
 }
 
-.update_local_repo <- function(
-        basedir, folder, new_branch = "devel", set_upstream = "origin/devel"
+#' @rdname update_local_repos
+#'
+#' @export
+update_local_repo <- function(
+        repo_dir, new_branch = "devel", set_upstream = "origin/devel"
 ) {
-    old_wd <- setwd(file.path(basedir, folder))
+    old_wd <- setwd(repo_dir)
     on.exit({ setwd(old_wd) })
     if (git_branch_exists(new_branch))
         return(git_branch_checkout(new_branch))
